969e5ee5c8ebe447b5d39b2e007378d9efb2682a
giovane <giovane_05silva@hotmail.com>
2022-07-08 11:03:34 -0300

    Função de ataque

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
 new Vue({
     el: '#app',
     data: {
-        botoes: false
+        botoes: false,
+        vidaJogador: 100,
+        vidaMonstro: 100
+    }, 
+    methods: {
+        ataque(){
+            numAleatorio = Math.ceil(Math.random() * 10)
+            numAleatorio2 = Math.ceil(Math.random() * 7)
+            this.vidaJogador =  this.vidaJogador - numAleatorio
+            this.vidaMonstro = this.vidaMonstro - numAleatorio2
+        }
     }
 })
