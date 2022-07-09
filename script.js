f0fbb5ee42dc4e88d1ee9fb2826b53fb1526d5f1
giovane <giovane_05silva@hotmail.com>
2022-07-09 08:53:33 -0300

    Função dos botões

--- script.js
+++ script.js
@@ -3,14 +3,26 @@ new Vue({
     data: {
         botoes: false,
         vidaJogador: 100,
-        vidaMonstro: 100
+        vidaMonstro: 100,
+        width: '50%'
     }, 
     methods: {
         ataque(){
-            numAleatorio = Math.ceil(Math.random() * 10)
-            numAleatorio2 = Math.ceil(Math.random() * 7)
-            this.vidaJogador =  this.vidaJogador - numAleatorio
-            this.vidaMonstro = this.vidaMonstro - numAleatorio2
+            danoJ = Math.ceil(Math.random() * 10)
+            danoM = Math.ceil(Math.random() * 7)
+            this.vidaJogador =  this.vidaJogador - danoJ
+            this.vidaMonstro = this.vidaMonstro - danoM
+        },
+        ataqueEspecial(){
+            danoJ = Math.ceil(Math.random() * 9)
+            danoM = Math.ceil(Math.random() * 10)
+            this.vidaJogador =  this.vidaJogador - danoJ
+            this.vidaMonstro = this.vidaMonstro - danoM
+        },
+        curar(){
+            curaJ = Math.ceil(Math.random() * 10)
+            danoM = Math.ceil(Math.random() * 9)
+            this.vidaJogador =  this.vidaJogador + (curaJ - danoM)
         }
     }
 })
